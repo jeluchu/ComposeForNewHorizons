6c9a61fcd89e1e4bc11ead80bb9c86484c20403d
Jéluchu <infojeluchu@gmail.com>
2022-04-16 15:11:28 +0200

    Include Scaffold in BugsView

--- app/src/main/java/com/jeluchu/composefornewhorizons/core/ui/theme/Color.kt
+++ app/src/main/java/com/jeluchu/composefornewhorizons/core/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val artichoke = Color(0xFFA29574)
+val cream = Color(0xFFF5F0CA)
